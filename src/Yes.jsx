5a09658fd2dfa678c56718c995eaaa8fb520fb39
John Rocha <xjohnfitcodes@gmail.com>
2024-11-27 13:33:08 -0500

    tailwindcss updated

--- src/Yes.jsx
+++ src/Yes.jsx
@@ -44,22 +44,24 @@ const Yes = () => {
 
     return (
         <div className="flex flex-col justify-center items-center">
-            <img className="" src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-            <div>
-                <div className="heart relative top-[70%]">
-                    <p className="z-10 rotate-45 relative right-0 left-6 text-center w-full lovers-quarrel-regular text-7xl">my forever woman</p>
-                    <p className="z-10 rotate-45 relative top-[13px] bottom-[-80px] right-[4rem] text-white text-center w-full font-semibold text-[0.7rem]">I Love You</p>
-                </div>
-
-            </div>
-            <span className="relative bottom-[-7rem] z-10 mt-[7rem] font-semibold">
+            <div className="flex flex-col justify-center items-center">
+            <span className="relative top-[-50px] z-10 font-semibold">
                 Time since I asked you to be my lil Chantal:
             </span>
-            <span className="boxShadowTime relative bottom-[-7rem] z-10 font-bold text-center text-4xl  italic border rounded-md m-3 p-2 ">
+            <span className="boxShadowTime relative z-10 top-[-50px] font-bold text-center text-xl italic border rounded-md m-3 p-2 ">
                 {
                     time ? time : updateTime()
                 }
             </span>
+            </div>
+                <img className="w-40 h-40 relative top-[-40px]" src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+            <div>
+                <div className="heart relative top-[55%]">
+                    <p className="z-10 rotate-45 relative right-0 left-6 text-center w-full lovers-quarrel-regular text-7xl">my forever woman</p>
+                    <p className="z-10 rotate-45 relative top-[13px] bottom-[-80px] right-[4rem] text-white text-center w-full font-semibold text-[0.7rem]">I Love You</p>
+                </div>
+
+            </div>            
         </div>
     );
 };
